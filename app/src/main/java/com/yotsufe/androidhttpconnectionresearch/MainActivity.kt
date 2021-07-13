e267b008a4160bd6278d5823448efe859fe15db9
yotsufe <yotsufe@gmail.com>
2021-07-14 02:51:38 +0900

    DataBinding導入 メニュー追加

--- app/src/main/java/com/yotsufe/androidhttpconnectionresearch/MainActivity.kt
+++ app/src/main/java/com/yotsufe/androidhttpconnectionresearch/MainActivity.kt
@@ -2,10 +2,22 @@ package com.yotsufe.androidhttpconnectionresearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.yotsufe.androidhttpconnectionresearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.kotlinCoroutine.setOnClickListener {
+        }
     }
 }
