08dbc042593e66c1f96bdd741d2ba49e5ebb2e73
yotsufe <yotsufe@gmail.com>
2021-07-14 12:34:43 +0900

    画面追加

--- app/src/main/java/com/yotsufe/androidhttpconnectionresearch/MainActivity.kt
+++ app/src/main/java/com/yotsufe/androidhttpconnectionresearch/MainActivity.kt
@@ -3,11 +3,13 @@ package com.yotsufe.androidhttpconnectionresearch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.yotsufe.androidhttpconnectionresearch.coroutine.KotlinCoroutineActivity
 import com.yotsufe.androidhttpconnectionresearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun initViews() {
         binding.kotlinCoroutine.setOnClickListener {
+            startActivity(KotlinCoroutineActivity.createIntent(this))
         }
     }
 }
---
+++ app/src/main/java/com/yotsufe/androidhttpconnectionresearch/coroutine/KotlinCoroutineActivity.kt
@@ -0,0 +1,42 @@
+package com.yotsufe.androidhttpconnectionresearch.coroutine
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import com.yotsufe.androidhttpconnectionresearch.R
+import com.yotsufe.androidhttpconnectionresearch.databinding.ActivityKotlinCoroutineBinding
+
+class KotlinCoroutineActivity : AppCompatActivity() {
+
+    companion object {
+        fun createIntent(context: Context) = Intent(context, KotlinCoroutineActivity::class.java)
+    }
+
+    private lateinit var binding: ActivityKotlinCoroutineBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_kotlin_coroutine)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+}
